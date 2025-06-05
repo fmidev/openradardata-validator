f50e094390f4f31c26fba3dd7772efa4ba0cfd2e
istvans <istvans@met.no>
2025-06-05 16:42:43 +0200

    Add data examples

---
+++ src/radar_cf.py
@@ -0,0 +1,99 @@
+radar_cf = {}
+radar_cf["TH"] = "radar_linear_equivalent_reflectivity_factor_h"
+radar_cf["TV"] = "radar_linear_equivalent_reflectivity_factor_v"
+radar_cf["DBZH"] = "radar_equivalent_reflectivity_factor_h"
+radar_cf["DBZV"] = "radar_equivalent_reflectivity_factor_v"
+# ODIM deprecated
+radar_cf["DBZ"] = "radar_equivalent_reflectivity_factor_h"
+
+radar_cf["ZDR"] = "radar_differential_reflectivity_hv"
+radar_cf["UZDR"] = "UZDR"
+# ODIM deprecated
+radar_cf["LZDR"] = "radar_differential_reflectivity_hv"
+
+radar_cf["RHOHV"] = "radar_correlation_coefficient_hv"
+
+radar_cf["URHOHV"] = ""
+
+radar_cf["LDR"] = "radar_linear_depolarization_ratio"
+radar_cf["ULDR"] = "ULDR"
+
+radar_cf["PHIDP"] = "radar_differential_phase_hv"
+radar_cf["UPHIDP"] = "UPHIDP"
+
+radar_cf["PIA"] = "PIA"
+radar_cf["KDP"] = "radar_specific_differential_phase_hv"
+radar_cf["UKDP"] = "UKDP"
+
+radar_cf["SQIH"] = "SQIH"
+radar_cf["USQIH"] = "USQIH"
+radar_cf["SQIV"] = "SQIV"
+radar_cf["USQIV"] = "USQIV"
+# ODIM deprecated
+radar_cf["SQI"] = "SQI"
+
+radar_cf["SNR"] = "radar_signal_to_noise_ratio"
+radar_cf["SNRHC"] = "radar_signal_to_noise_ratio_copolar_h"
+radar_cf["SNRHX"] = "radar_signal_to_noise_ratio_crosspolar_h"
+radar_cf["SNRVC"] = "radar_signal_to_noise_ratio_copolar_v"
+radar_cf["SNRVX"] = "radar_signal_to_noise_ratio_crosspolar_v"
+radar_cf["USNRHC"] = "USNRHC"
+radar_cf["USNRVC"] = "USNRVC"
+
+radar_cf["CCORH"] = "CCOR_h"
+radar_cf["CCORV"] = "CCOR_v"
+
+radar_cf["CPA"] = "CPA"
+
+radar_cf["RATE"] = "radar_estimated_precipitation_rate"
+radar_cf["URATE"] = "URATE"
+
+radar_cf["POR"] = "POR"
+radar_cf["HI"] = "HI"
+radar_cf["POH"] = "POH"
+radar_cf["POSH"] = "POSH"
+
+radar_cf["MESH"] = "MESH"
+
+radar_cf["ACRR"] = "ACRR"
+radar_cf["HGT"] = "HGT"
+radar_cf["VIL"] = "VIL"
+
+radar_cf["VRADH"] = "radial_velocity_of_scatterers_away_from_instrument_h"
+radar_cf["UVRADH"] = "UVRADH"
+radar_cf["VRADV"] = "radial_velocity_of_scatterers_away_from_instrument_v"
+radar_cf["UVRADV"] = "UVRADV"
+# ODIM deprecated
+radar_cf["VRAD"] = "radial_velocity_of_scatterers_away_from_instrument_h"
+
+radar_cf["VRADDH"] = "VRADDH"
+radar_cf["VRADDV"] = "VRADDV"
+
+radar_cf["WRADH"] = "radar_doppler_spectrum_width_h"
+radar_cf["UWRADH"] = "UWRADH"
+radar_cf["WRADV"] = "radar_doppler_spectrum_width_v"
+radar_cf["UWRADV"] = "UWRADV"
+# ODIM deprecated
+radar_cf["WRAD"] = "radar_doppler_spectrum_width_h"
+
+radar_cf["UWND"] = "UWND"
+radar_cf["VWND"] = "VWND"
+
+radar_cf["RSHR"] = "RSHR"
+radar_cf["ASHR"] = "ASHR"
+radar_cf["CSHR"] = "CSHR"
+radar_cf["ESHR"] = "ESHR"
+radar_cf["OSHR"] = "OSHR"
+radar_cf["HSHR"] = "HSHR"
+radar_cf["VSHR"] = "VSHR"
+radar_cf["TSHR"] = "TSHR"
+radar_cf["PSH"] = "PSH"
+radar_cf["PSV"] = "PSV"
+
+radar_cf["UPSPH"] = "UPSPH"
+radar_cf["UPSPV"] = "UPSPV"
+
+radar_cf["BRDR"] = "BRDR"
+radar_cf["QIND"] = "QIND"
+
+radar_cf["CLASS"] = "CLASS"
